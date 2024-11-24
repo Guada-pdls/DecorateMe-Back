0d1d40ccb04927cd926f7f163819403400b6611a
Guada <guadapdls@gmail.com>
2024-11-24 20:21:28 -0300

    testing possible memory problems

--- src/server.js
+++ src/server.js
@@ -3,37 +3,23 @@ import app from "./app.js";
 import config from "./config/config.js";
 import { logger } from "./utils/logger.js";
 import chat from "./utils/chat.js";
-import cluster from "node:cluster"
-import { cpus } from "node:os"
-import http from "http"
+import http from "http";
 
-if (cluster.isPrimary) {
-  const processors = cpus().length;
+const httpServer = http.createServer(app);
 
-  for (let i = 0; i < processors; i++) {
-    cluster.fork();
-  }
+httpServer.listen(config.PORT, () => {
+  logger.info("Server listening on port " + config.PORT);
+});
 
-  cluster.on('message', (worker, code, signal) => {
-    logger.error(`Message received from worker ${worker.process.pid}`);
-  });
-} else {
-  const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: config.FRONT_DOMAIN,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  },
+});
 
-  httpServer.listen(config.PORT, () => {
-    logger.info("Server listening on port " + config.PORT);
-  });
-
-  const io = new Server(httpServer, {
-    cors: {
-      origin: config.FRONT_DOMAIN,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true
-    }
-  });
-
-  io.on('connection', async (socket) => {
-    logger.info(`client ${socket.client.id} connected`);
-    chat(socket, io);
-  });
-}
+io.on("connection", async (socket) => {
+  logger.info(`client ${socket.client.id} connected`);
+  chat(socket, io);
+});
