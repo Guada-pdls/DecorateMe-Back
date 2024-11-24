9f7c7eb4dfcd955bbbba762fcf01dd8cd0254169
Guada <guadapdls@gmail.com>
2024-11-24 20:04:08 -0300

    import and initializing MongoStore correction

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import express from "express";
 import "dotenv/config.js";
+import cors from "cors";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import compression from "express-compression";
+import connectMongo from "connect-mongo";
+
 import errorHandler from "./middlewares/error/errorHandler.js";
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 import { __dirname } from "./utils/utils.js";
 import router from "./routes/index.js";
-import cors from "cors";
-import passport from "passport";
 import inicializePassport from "./passport-jwt/passport.config.js";
-import cookieParser from "cookie-parser";
 import config from "./config/config.js";
 import { addLogger } from "./utils/logger.js";
-import session from "express-session";
-const MongoStore = require('connect-mongo')(session);
-import compression from "express-compression";
 
 const server = express();
 config.connectDB();
@@ -37,15 +38,19 @@ server.use(compression({
 inicializePassport();
 server.use(passport.initialize());
 
-server.use(session({
-  secret: config.SECRET_SESSION,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true },
-  store: new MongoStore({
-    checkPeriod: 86400000
+server.use(
+  session({
+    secret: config.SECRET_SESSION,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, 
+    store: new connectMongo({
+      mongoUrl: config.MONGO_LINK, 
+      collectionName: "sessions",
+      ttl: 86400,
+    }),
   })
-}));
+)
 
 server.use(addLogger)
 
